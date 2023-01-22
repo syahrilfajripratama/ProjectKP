76cb19fc82a7387d6824831cc3f6c08b000705c8
syahrilfajripratama <syahril.fajripratama2017@students.unila.ac.id>
2023-01-22 19:41:58 +0700

    Ganti daru Native ke Framework Laravel

--- src/conn.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$mysqli = new mysqli("localhost","root","","kp_db");
-
-// Check connection
-if (!$mysqli) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-?>
